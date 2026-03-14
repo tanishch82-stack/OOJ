0d2438b3a6e627c45074a413b5f4cc820828ce6d
tanishch82-stack <tanishch82@gmail.com>
2026-03-14 11:17:08 +0530

    Add files via upload

---
+++ generated/client/nodes/3.js
@@ -0,0 +1,1 @@
+export { default as component } from "../../../../src/routes/border-select/+page.svelte";
---
+++ generated/client/nodes/6.js
@@ -0,0 +1,1 @@
+export { default as component } from "../../../../src/routes/exit-monitoring/+page.svelte";
